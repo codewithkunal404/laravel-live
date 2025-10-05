90b8501698cf8e7e82c4fcef729c114219e19ba4
Your Name <deepakchaudhary1004@gmail.com>
2025-10-05 20:30:29 +0530

    Fix line endings to LF

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('tasks.view');
 });
 
 Route::get('/task-list',[TaskController::class,'index'])->name("tasks.view");
